93c0ac1c4273b50c5c5361619043e720ed29f5af
Jake Dunafon <jlad795@gmail.com>
2022-01-05 21:37:07 -0800

    Refactor input errors

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ var bodyError = document.querySelector(".error-body");
 
 
 newSave.addEventListener('click', newIdea);
+newSave.addEventListener('hover', error);
+
+newTitle.addEventListener('input', error);
+newBody.addEventListener('input', error);
+
+function error(){
+  if (newBody.value && newTitle.value){
+    newSave.classList.remove("not-allowed")
+  }
+}
 
 function newIdea() {
   event.preventDefault();
@@ -26,28 +36,30 @@ function newIdea() {
   }
 
   if (newBody.value && newTitle.value){
-  var newIdea = new Idea(newTitle.value, newBody.value);
-  savedIdeas.push(newIdea);
 
-  ideaBoxSection.innerHTML +=
-    `<section class="idea-box">
-      <div class="box-header">
-        <img class="box-images hidden" src="./assets/star-active.svg" alt=""/>
-        <img class="box-images" src="./assets/star.svg" alt=""/>
-        <img class="box-images" src="./assets/delete.svg" alt=""/>
-        <img class="box-images hidden" src="./assets/delete-active.svg" alt=""/>
-      </div>
-      <div class="box-body">
-        <h1 class= 'idea-title'>${newIdea.title}</h1>
-        <p class= 'idea-body'>${newIdea.body}</p>
-      </div>
-      <div class="box-footer">
-        <img class="box-images" src="./assets/comment.svg" alt=""/>
-    <h4 class="comment">Comment</h4>
-      </div>
-    </section>`
+    var newIdea = new Idea(newTitle.value, newBody.value);
+    savedIdeas.push(newIdea);
+
+    ideaBoxSection.innerHTML +=
+      `<section class="idea-box">
+        <div class="box-header">
+          <img class="box-images hidden" src="./assets/star-active.svg" alt=""/>
+          <img class="box-images" src="./assets/star.svg" alt=""/>
+          <img class="box-images" src="./assets/delete.svg" alt=""/>
+          <img class="box-images hidden" src="./assets/delete-active.svg" alt=""/>
+        </div>
+        <div class="box-body">
+          <h1 class= 'idea-title'>${newIdea.title}</h1>
+          <p class= 'idea-body'>${newIdea.body}</p>
+        </div>
+        <div class="box-footer">
+          <img class="box-images" src="./assets/comment.svg" alt=""/>
+      <h4 class="comment">Comment</h4>
+        </div>
+      </section>`
+
     titleError.classList.add("hidden");
     bodyError.classList.add("hidden");
   }
-
+  console.log(savedIdeas)
 };
